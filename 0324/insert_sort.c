5ae32c3628e5bed3bf2d20d87c1e8124c787f7dc
nam2626 <nam2626@gmail.com>
2021-03-25 21:26:11 +0900

    삽입 정렬 완료

--- 0324/insert_sort.c
+++ 0324/insert_sort.c
@@ -10,13 +10,15 @@ int main(void) {
 	int arr[6];
 	int i, j;
 	//삽입 정렬 수행
-	for (i = ; i < ; i++) {
+	for (i = 0; i < 6; i++) {
 		//입력
 		printf("숫자 입력 : ");
 		scanf_s("%d", &arr[i]);
 		//정렬 수행
-		for (j = ; j; j--) {
-
+		for (j = i; j>0 && arr[j] < arr[j - 1]; j--) {
+				int temp = arr[j];
+				arr[j] = arr[j - 1];
+				arr[j - 1] = temp;
 		}
 		PrintArray(arr, i + 1);
 	}
