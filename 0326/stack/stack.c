c52509df86cf8f80094346247c654cfdc6e104d8
nam2626 <nam2626@gmail.com>
2021-03-26 19:28:58 +0900

    stack 작업 준비

---
+++ 0326/stack/stack.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+void Init(int size) {
+	//stack 초기화
+}
+void Push(int value) {
+	//데이터를 등록
+}
+int Pop(int value) {
+	//데이터를 뽑음
+	return 0;
+}
---
+++ 0326/stack/stack.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef __STACK_H__
+#define __STACK_H__
+typedef struct stack {
+	int top;
+	int size;
+	int* s;
+}Stack;
+
+Stack stack;
+//스택 초기화 하는 부분
+void Init(int size);
+//스택에 데이터를 추가하는 부분
+void Push(int value);
+//스택에 있는 데이터를 뽑는 부분
+int Pop(int value);
+#endif
