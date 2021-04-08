a9202780b8cd32def9b16a5d4cf385912f43f25c
NamSangKyu <nam2626@gmail.com>
2021-04-08 21:45:13 +0900

    message passing

--- 0408/message_passing.cpp
+++ 0408/message_passing.cpp
@@ -4,24 +4,6 @@
 */
 #include <iostream>
 using namespace std;
-class Person {
-private:
-	int money;
-public:
-	Person() {
-		money = 10000;
-	}
-	void takeBus(Bus &bus) {
-		bus.creditMoney(1300);
-		money -= 1300;
-		cout << "Person Bus에 탑승했습니다" << endl;
-	}
-	void takeSubway(Subway& sub) {
-		sub.creditMoney(1450);
-		money -= 1300;
-		cout << "Person Subway에 탑승했습니다" << endl;
-	}
-};
 class Bus {
 private:
 	int money;
@@ -58,3 +40,47 @@ public:
 		cout << "지하철 탑승 누적 인원수 : " << count << endl;
 	}
 };
+
+class Person {
+private:
+	int money;
+public:
+	Person() {
+		money = 10000;
+	}
+	void takeBus(Bus& bus) {
+		bus.creditMoney(1300);
+		money -= 1300;
+		cout << "Person Bus에 탑승했습니다" << endl;
+	}
+	void takeSubway(Subway& sub) {
+		sub.creditMoney(1450);
+		money -= 1450;
+		cout << "Person Subway에 탑승했습니다" << endl;
+	}
+	void PrintPersonInfo() {
+		cout << "현재 Person 남은 금액 : " << money << endl;
+	}
+};
+int main(void) {
+	Person p1, p2, p3;
+	Subway sub;
+	Bus bus;
+
+	p1.takeBus(bus);
+	p1.takeBus(bus);
+	p1.takeBus(bus);
+	p1.takeSubway(sub);
+	p2.takeBus(bus);
+	p2.takeSubway(sub);
+	p3.takeBus(bus);
+	p3.takeSubway(sub);
+	p3.takeSubway(sub);
+	p3.takeSubway(sub);
+	p3.takeSubway(sub);
+	sub.printInfo(); bus.printInfo();
+	p1.PrintPersonInfo();
+	p2.PrintPersonInfo();
+	p3.PrintPersonInfo();
+	return 0;
+}
